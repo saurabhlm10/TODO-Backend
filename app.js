758f539dc67775dc1036e0e51d63bb32ae5f250c
saurabhlm10 <saurabhlm10@gmail.com>
2023-01-08 10:44:27 +0530

    added cors

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const connectToDb = require('./config/db')
+const cors = require('cors');
+
 
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors());
+
 
 connectToDb()
 
